8d8747fbcf9bcbef72e9f643e8b282ec50ef7bcf
Joshua bingham <binghamjw@g.cofc.edu>
2018-11-20 10:48:49 -0500

    Adding Some Unit Tests

--- RussianHackers_MidtermElectionApp/src/com/csci360/electionapp/model/JUnitTests.java
+++ RussianHackers_MidtermElectionApp/src/com/csci360/electionapp/model/JUnitTests.java
@@ -8,11 +8,13 @@ import org.junit.jupiter.api.Test;
 public class JUnitTests {
 
     public Voter v;
+    public Election e;
 
 
     @BeforeEach
     public void setUp() throws Exception {
         v = new Voter("Tyler", "1");
+        e = new Election();
     }
 
     @Test
@@ -25,5 +27,12 @@ public class JUnitTests {
     public void allowVoter(){
         v.allowToVote();
         assertEquals(true, v.checkedIfCanVote());
+    }
+
+    @Test
+    public void testAddOffice(){
+        Office o = new Office("President");
+        e.addOffice(o);
+        assertEquals(true,e.contains(o));
     }
 }
