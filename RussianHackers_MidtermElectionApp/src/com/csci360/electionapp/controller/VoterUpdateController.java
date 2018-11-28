7b97e2120bb1195437980bd453fbc03e31c5262b
Tyler Gray <grayt@g.cofc.edu>
2018-11-28 18:36:20 -0500

    Completed.
    Finished everything I can. Got the inputs hopefully validated. Fixed all bugs I could find. Trying to export to jar now.

--- RussianHackers_MidtermElectionApp/src/com/csci360/electionapp/controller/VoterUpdateController.java
+++ RussianHackers_MidtermElectionApp/src/com/csci360/electionapp/controller/VoterUpdateController.java
@@ -4,6 +4,7 @@ import com.csci360.electionapp.foundation.MySQLAccess;
 import com.csci360.electionapp.tech.security.Encryption;
 import com.gluonhq.charm.glisten.control.TextField;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 
 import java.util.ArrayList;
 
@@ -11,7 +12,7 @@ public class VoterUpdateController {
 
     private Main main;
     private String voterID;
-
+    private String regex;
     private ArrayList<TextField> fields;
 
     @FXML
@@ -50,12 +51,72 @@ public class VoterUpdateController {
         ssn.setText(info.get(5));
         dlnumber.setText(info.get(6));
 
+        fields = new ArrayList<>();
+        fields.add(firstname);
+        fields.add(lastname);
+        fields.add(address);
+        fields.add(zipcode);
+        fields.add(ssn);
+        fields.add(dlnumber);
+        regex = "^[0-9]{9}";
+
+
 
 
     }
 
     @FXML
     void submitClicked() throws Exception {
+
+        boolean empty = false;
+        for(TextField f: fields){
+            f.setStyle(null);
+            if(f.getText().trim().isEmpty()){
+                f.setStyle("    -fx-text-box-border: red ;\n" +
+                        "    -fx-focus-color: red ;");
+                empty = true;
+
+            }
+
+
+        }
+        if(empty){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+
+            alert.setTitle("Blank Fields");
+            alert.setHeaderText("Please make sure you fill out the entire form in order to sumbit.");
+            alert.showAndWait();
+            return;
+        }
+
+        if(!ssn.getText().matches(regex)){
+            ssn.setStyle("    -fx-text-box-border: red ;\n" +
+                    "    -fx-focus-color: red ;");
+
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+
+            alert.setTitle("SSN must be 9 digits");
+            alert.setHeaderText("Please make sure your social security is 9 digits.");
+            alert.showAndWait();
+            ssn.setText("");
+            return;
+        }
+
+        if(!dlnumber.getText().matches(regex)){
+            dlnumber.setStyle("    -fx-text-box-border: red ;\n" +
+                    "    -fx-focus-color: red ;");
+
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+
+            alert.setTitle("DL Number must be 9 digits");
+            alert.setHeaderText("Please make sure your DL Number is 9 digits.");
+            alert.showAndWait();
+            dlnumber.setText("");
+            return;
+        }
+
+
+
         ArrayList<String> info = new ArrayList<>();
         info.add(voterID);
         info.add(firstname.getText());
